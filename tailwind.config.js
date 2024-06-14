559f0826ea8440230a793b8e67aec2870fbf0803
matix1000000 <matix1000000@gmail.com>
2024-06-14 18:46:35 +0200

    Konfiguracja na mój PC

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './**/*.{html,js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
